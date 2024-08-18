c009c69ff1a77704645d587d4f3fb2f264f8d715
Hyunwoo <nhw3990@kau.kr>
2024-08-18 22:07:10 +0900

    Counter 앱 UI 구현 완료

---
+++ src/components/Controller.jsx
@@ -0,0 +1,14 @@
+const Contorller = () => {
+  return (
+    <div>
+      <button>-1</button>
+      <button>-10</button>
+      <button>-100</button>
+      <button>+100</button>
+      <button>+10</button>
+      <button>+1</button>
+    </div>
+  );
+};
+
+export default Contorller;
